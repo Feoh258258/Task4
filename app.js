baf6218775fbed7c63481096fca2387bd6754c04
Feoh258258 <65220078+Feoh258258@users.noreply.github.com>
2020-05-19 19:54:35 +0300

    HW04-2UP

--- app.js
+++ app.js
@@ -20,3 +20,63 @@ let newMas= mas1.concat(mas2);
 console.log(newMas);
 let newMasSplice = mas1.slice(0) + mas2.slice(0);
 console.log(newMasSplice);
+
+//Task5
+
+  
+document.addEventListener("DOMContentLoaded", function() {
+  const onlyBtn = document.querySelectorAll('.changeColor');
+  for (let i =0; i<onlyBtn.length; i++){
+    onlyBtn[i].onclick = function(e) {
+      document.body.style.backgroundColor  = getComputedStyle(e.target).getPropertyValue("background-color")
+    } 
+  }
+
+//Тask 2
+
+const ticTacToeArr = [
+  [0, 0, 0],
+  [0, 1, 2],
+  [2, 1, 0]
+];
+
+//ЧТО НЕ ТАК ПО ЛОГИКЕ?
+
+for(let i=0; i<ticTacToeArr.length; i++){
+  let sumArr=0;
+  // for (let j=0; j<ticTacToeArr.length; j++){
+    sumArr += ticTacToeArr[i];
+  // }//f2f
+  console.log(sumArr);
+}
+
+
+
+
+
+
+
+
+
+
+
+//Таск 4
+// let taskTwoArr = ['Яблоко','Банан', 'Ананас'];
+//   taskTwoArrSome= taskTwoArr.map(el=>el[0]);
+//   console.log(taskTwoArrSome);
+
+//   function taskTwoArrSum(a, b){
+//     return a[0]+b[0];
+//     }
+//     let s=taskTwoArr.reduce(taskTwoArrSum);
+
+
+//  taskTwoArr.reduce(function(prevValue, currentValue){
+//     console.log(prevValue[0] + currentValue[0]);
+//     return prevValue + currentValue
+//   });
+
+
+
+
+});//End DOMContentLoaded
